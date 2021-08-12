9aa1a4f261c23628572ed0db109dcef844cc8863
Ravi Bharti <ravics1718@gmail.com>
2021-08-12 18:34:52 +0530

    Renamed files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Home from "./components/Home";
+import Home from "./components/homes";
 import Nf404 from "./components/404";
 
 function App() {
